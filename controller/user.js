7c4f567cac76861805721f774005e5f4f1bed23f
Remons <remons@foxmail.com>
2023-10-30 11:03:19 +0800

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -81,6 +81,26 @@ const addUser = async (ctx) => {
     ctx,
     method: 'POST',
   });
+  let userId = '';
+  if (ctx.headers.remons_token) {
+    userId = varifyToken(ctx.headers.remons_token).id;
+  }
+  let sqlUserId = `select * from user where id='${userId}'`;
+  const userResult = await search({ sql: sqlUserId });
+  let role = '';
+  if (userResult.data.length) {
+    if ((userResult.data[0].role || '').includes('admin')) {
+      role = 'admin';
+    }
+  }
+  if (role !== 'admin') {
+    ctx.body = {
+      code: 403,
+      success: false,
+      msg: '无权限',
+    };
+    return;
+  }
   let searchSql = `select * from user where 1=1 and account='${account}'`;
   const result = await search({ sql: searchSql });
   if (result.data.length) { 
